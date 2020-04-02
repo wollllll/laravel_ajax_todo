e44d25e1144e8dbbfa19e1e5af4df8c16d7afa69
wollllll <sakin50guramu@gmail.com>
2020-04-02 23:56:49 +0900

    作業途中

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TaskService;
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request, TaskService $service)
+    {
+        $task = $service->store($request);
+
+        return response(view('tasks.test'));
+    }
+
+    public function update(Request $request, Task $task, TaskService $service)
+    {
+        $updatedTask = $service->update($request, $task);
+
+        return response($updatedTask);
+    }
+}
