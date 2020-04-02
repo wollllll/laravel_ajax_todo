f2e41d67d010a89a0048fcc7fd791ffbf955c8ac
wollllll <sakin50guramu@gmail.com>
2020-04-03 01:52:32 +0900

    viewの分離

---
+++ resources/views/layouts/_base.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>俺のTODO</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body class="my-5">
+@yield('content')
+
+@include('layouts._js-footer')
+</body>
+</html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,39 +1,19 @@
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>todo</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-</head>
-<body class="my-5">
-<div class="container w-75">
-    <h1>俺のTODO</h1>
-    <div class="mb-5">
-        <textarea class="form-control" name="store-content" rows="6"></textarea>
-        <div class="w-50 mx-auto mt-5">
-            <input type="submit" id="store-btn" class="btn btn-outline-primary btn-block"
-                   data-store-url="{{ route('tasks.store') }}" value="記録">
+@extends('layouts._base')
+
+@section('content')
+    <div class="container w-75">
+        <h1>俺のTODO</h1>
+        <div class="mb-5">
+            <textarea class="form-control" name="store-content" rows="6"></textarea>
+            <div class="w-50 mx-auto mt-5">
+                <input type="submit" id="store-btn" class="btn btn-outline-primary btn-block"
+                       data-store-url="{{ route('tasks.store') }}" value="記録">
+            </div>
         </div>
+        <ul id="task-lists" class="row">
+            @foreach($tasks as $task)
+                @include('tasks.list')
+            @endforeach
+        </ul>
     </div>
-    <ul id="task-lists" class="row">
-        @foreach($tasks as $task)
-            @include('tasks.list')
-        @endforeach
-    </ul>
-</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-<script src="{{ asset('js/main.js') }}"></script>
-</body>
-</html>
+@endsection
